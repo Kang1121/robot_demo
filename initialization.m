0ca9bdf67fea4cc9c088234881c7f13114360bec
heybshin <57985020+heybshin@users.noreply.github.com>
2023-03-31 01:49:17 +0900

    Update initialization.m
    Add goToHomePosition and sendFingerPositionCommand to set robot arm to default configuration (home position, open fingers)

--- initialization.m
+++ initialization.m
@@ -1,9 +1,16 @@
-function [jc] = initialization()
+function [robot] = initialization()
 
-    jc = JacoComm;
-    connect(jc);
-    calibrateFingers(jc);
-    setPositionControlMode(jc);
+    % Initialize robot
+    robot = JacoComm;
+    connect(robot);
+
+    % Initialize fingers to default ('open')
+    calibrateFingers(robot);
+    sendFingerPositionCommand(robot,[0;0;0]);
+
+    % Initialize position to default ('home')
+    setPositionControlMode(robot);
+    goToHomePosition(robot);
+    disp(robot.EndEffectorPose);
 
 end
-
