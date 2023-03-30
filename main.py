e3619038c45c431dcda40268966b9176ff8e5ff0
heybshin <57985020+heybshin@users.noreply.github.com>
2023-03-31 01:41:06 +0900

    Update based on fixed scenario

--- main.py
+++ main.py
@@ -1,71 +1,87 @@
 """
-Some tested values for individual joint movement only (exceeding value assignment may cause danger)
-Joint 1 --> no limit
-Joint 2 --> 1.0 ~ 4.2
-Joint 3 --> no limit
-Joint 4 --> 0.5 ~ 5.5
-Joint 5 --> no limit
-Joint 6 --> 1.2 ~ 5.2
-Joint 7 --> no limit
+This program executes the following robotic arm control scenario:
+- Reach and grasp water bottle -> Pour water into cup -> Bring cup to user
+
+To change the scenario, modify the following in this code snippet:
+- order and type of actions (reach, grasp, twist)
+- length of pauses in-between actions ('lag' is in seconds)
+- angle of twist ('angle' is in degrees. modify with care.)
+
+To change or add objects, modify the config (yaml) file and this code accordingly.
 """
-
-from __future__ import division, print_function, absolute_import, unicode_literals
-import time
-import numpy as np
-from utils import *
-from collections import OrderedDict
-
+import argparse
+from module_ex import *
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Robotic Arm Control')
+    parser.add_argument('--preset', type=str, default='preset_ex', help='name of preset config file')
+    parser.add_argument('--api', type=str, default='matlab_api', help='name of matlab api directory')
+    parser.add_argument('--verbose', action='store_true', help='robot likes to talk')
+    args = parser.parse_args()
 
-    # Initialize Robotic Arm API and move to Home to stand by
-    robotic_arm = api_init()
-    # des_pos = np.round(np.array(get_angular_pos(robotic_arm)))
-    # print(des_pos)
+    # INITIALIZATION
+    robot = RobotControlModule(args)
 
-    # move(robotic_arm, py2mat([[283.23], [162.70], [0], [43.59], [265.23], [257.52], [288.14]]), 0)
-    # move(robotic_arm, py2mat([[0], [0], [0]]), 0)
+    xyz = {'standby': [-0.09, -0.5, 0.07], 'user': [], } #, 'bottle': [], 'cup': []}
+    robot.reach(cmd=xyz['standby'], lag=4)
+    robot.twist(cmd='left', angle=180, lag=6)
 
-    # # Move arm to home position
-    # start = time.time()
-    # print('Time elapsed for initialization: ', time.time() - start)
+    # EXECUTION
+    # 1. Select object based on EEG classification result
+    # TODO: cls = 'bottle' -> input to AR module
 
-    # print('\nTell me what to do: "home", "end"...')
-    # user_input = input()
+    # 2. Move close to object (bottle) using AR coordinates
+    xyz['bottle'] = [0.15, -0.45, 0.1] # output of AR module
+    robot.reach(cmd=xyz['bottle'], lag=4)
 
-    # while user_input != 'end':
-    #     if user_input == 'automation':
-    #         pos = get_pos('preset')
-    #         pos = OrderedDict(reversed(list(pos.items())))
-    #
-    #         while bool(pos):
-    #             cmd = pos.popitem()[1]
-    #             interval = cmd.pop()
-    #             move(robotic_arm, py2mat(cmd), interval)
-    #
-    #     else:
-    #         pos = get_pos('positions')
-    #         if user_input == 'home':
-    #             move(robotic_arm, py2mat(pos['home']), 0)
-    #         else:
-    #             pass
-    #     # Destination position
-    #     # des_pos = get_angular_pos(robotic_arm)
-    #     # print(des_pos)
-    #     # Get new position inputs
-    #     print('\n I am moving... In the meantime, you can tell me what to do next!:')
-    #     user_input = input()
+    # 3. Adjust based on EEG classification result
+    cls = 'left'  # output of EEG module
+    x = -0.05 if cls == 'right' else 0.05
+    robot.adjust(cmd=[x, 0, 0], lag=4)
 
-    # End of program
-    # print('Bye!')
+    # 4. Grasp object based on EEG classification result
+    # Assume output was 'grasp' (true)
+    robot.grasp(cmd='close', lag=3)
 
-    pos = OrderedDict(reversed(list(get_pos('preset').items())))
+    # 5. Move close to object (cup) using AR coordinates
+    xyz['cup'] = [0.35, -0.35, 0.1]
+    pour_pos = xyz['cup'] + [0, 0, 0.1]
+    robot.reach(cmd=pour_pos, lag=2)
 
-    while bool(pos):
-        name, cmd = pos.popitem()
-        print(name)
-        print('-' * 30)
-        sleep, interval = cmd.pop(), cmd.pop()
-        move(robotic_arm, py2mat(cmd), interval, sleep)
+    # 6. Adjust based on EEG classification result
+    cls = 'right'
+    x = -0.05 if cls == 'right' else 0.05
+    robot.adjust(cmd=[x, 0, 0], lag=4)
 
-    robotic_arm[0].quit()
+    # 7. Twist object (bottle) based on EEG classification result
+    # Assume output was 'twist' (true)
+    robot.twist(cmd='left', angle=300, lag=6)
+    robot.twist(cmd='right', angle=280, lag=3)
+
+    # 8. Put object (bottle) back in original position
+    robot.reach(cmd=xyz['bottle'], lag=4)
+    robot.grasp(cmd='open', lag=3)
+
+    # 9. Move close to object (cup) using AR coordinates
+    robot.reach(cmd=xyz['cup'], lag=2)
+
+    # 10. Adjust based on EEG classification result
+    cls = 'right'
+    x = -0.05 if cls == 'right' else 0.05
+    robot.adjust(cmd=[x, 0, 0], lag=4, delta=True)
+
+    # 11. Grasp object (cup) based on EEG classification result
+    # Assume output was 'grasp' (true)
+    robot.grasp(cmd='close', lag=3)
+
+    # 12. Bring cup to user and tilt cup
+    robot.reach(cmd=xyz['user'], lag=3)
+    robot.twist(cmd='left', angle=180, lag=6)
+    robot.twist(cmd='right', angle=160, lag=3)
+
+    # 13. Put object (cup) back in original position
+    robot.reach(cmd=xyz['cup'], lag=2)
+    robot.grasp(cmd='open', lag=3)
+
+    # 14. Return to home position
+    robot.rest()
